9ab24fad0113b441ba8be79c659c636ff494d168
Hugo Hromic <hhromic@gmail.com>
2021-07-07 01:02:08 +0100

    renamed main module as per NodeJS recommendations

--- server.js
+++ server.js
@@ -12,6 +12,8 @@
 //   See the License for the specific language governing permissions and
 //   limitations under the License.
 
+'use strict';
+
 const http = require('http'),
       httpProxy = require('http-proxy');
 
